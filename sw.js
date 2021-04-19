3a897b2dd9c0b1d1d57db69f3ee93cef47fd015c
Lesther <jordi.lf.cb@gmail.com>
2021-04-18 20:00:47 -0400

    cambios notch y dynamic

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 //Import
 importScripts("js/sw-utils.js");
 
-const STATIC_CACHE = "static-v2";
-const DYNAMIC_CACHE = "dynamic-v1";
+const STATIC_CACHE = "static-v4";
+const DYNAMIC_CACHE = "dynamic-v2";
 const INMUTABLE_CACHE = "inmutable-v1";
 
 const APP_SHELL = [
@@ -42,8 +42,10 @@ self.addEventListener("install", (e) => {
 self.addEventListener("activate", (e) => {
   const respuesta = caches.keys().then((keys) => {
     keys.forEach((key) => {
-      console.log(key);
       if (key !== STATIC_CACHE && key.includes("static")) {
+        return caches.delete(key);
+      }
+      if (key !== DYNAMIC_CACHE && key.includes("dynamic")) {
         return caches.delete(key);
       }
     });
